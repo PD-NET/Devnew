bf6e540b24ec1f08e9140bffb0fbbb4a4f79ca54
manojnaik12 <141448013+manojnaik12@users.noreply.github.com>
2024-01-14 23:24:37 +0530

    Update security.py

--- security.py
+++ security.py
@@ -5,15 +5,13 @@ from zapv2 import ZAPv2
 target = 'http://pd-net-prod-main-1518350640.us-east-1.elb.amazonaws.com/'
 # Change to match the API key set in ZAP, or use None if the API key is disabled
 apiKey = '5h42cgakvq6ovrsm7rd6dtm2rj'
-proxies = {'http': 'http://localhost:8081/'}
-zap = ZAPv2(apikey=apiKey, proxies=proxies)
-# By default, ZAP API client will connect to port 8080
+
 # Use the line below if ZAP is not listening on port 8080, for example, if listening on port 8090
+zap = ZAPv2(apikey=apiKey, proxies={'http': 'http://localhost:8081/'})
 
 print('Spidering target {}'.format(target))
 # The scan returns a scan id to support concurrent scanning
 scanID = zap.spider.scan(target)
-print(scanID)
 while int(zap.spider.status(scanID)) < 100:
     # Poll the status until it completes
     print('Spider progress %: {}'.format(zap.spider.status(scanID)))
@@ -22,4 +20,3 @@ while int(zap.spider.status(scanID)) < 100:
 print('Spider has completed!')
 # Prints the URLs the spider has crawled
 print('\n'.join(map(str, zap.spider.results(scanID))))
-# If required post process the spider results
