a4e4d10225da799804ba967957df73cad7dcf0a7
manojnaik12 <141448013+manojnaik12@users.noreply.github.com>
2024-01-15 00:10:03 +0530

    Update zap_script.py

--- zap_script.py
+++ zap_script.py
@@ -3,7 +3,7 @@ import time
 from zapv2 import ZAPv2
 
 # ZAP API URL
-zap_api_url = os.getenv('ZAP_API_URL', 'http://localhost:8081/')  # Update with your ZAP API URL
+zap_api_url = os.getenv('ZAP_API_URL', 'http://localhost:8080')  # Update with your ZAP API URL
 
 # ZAP API Key
 api_key = os.getenv('ZAP_API_KEY', '5h42cgakvq6ovrsm7rd6dtm2rj')  # Update with your ZAP API key
@@ -13,13 +13,13 @@ proxy_url = os.getenv('HTTP_PROXY', 'http://localhost:8081/')  # Update with you
 proxies = {'http': proxy_url}
 
 # Initialize ZAP
-zap = ZAPv2(apikey=api_key, baseurl=zap_api_url, proxies=proxies)
+zap = ZAPv2(apikey=api_key, proxies=proxies)
 
 # Your ZAP-related actions here
 # For example, you can run a spider scan or an active scan
 
 # Example: Spider the target URL
-target_url = 'http://example.com'
+target_url = 'http://pd-net-prod-main-1518350640.us-east-1.elb.amazonaws.com/'
 scan_id = zap.spider.scan(target_url)
 
 print(f'Spidering target {target_url}...')
