67d1add62a026e1feb49728c3573fd9012454c0f
manojnaik12 <141448013+manojnaik12@users.noreply.github.com>
2024-01-15 00:19:41 +0530

    Update zap_script.py

--- zap_script.py
+++ zap_script.py
@@ -7,25 +7,17 @@ proxy_url = os.getenv('HTTP_PROXY', 'http://localhost:8081/')  # Update with you
 proxies = {'http': proxy_url}
 
 print(f"Attempting to connect to the proxy: {proxy_url}")
+response = requests.get('http://pd-net-prod-main-1518350640.us-east-1.elb.amazonaws.com/', proxies=proxies)
+print(f"Proxy connection successful. Response: {response.text}")
+api_key = os.getenv('ZAP_API_KEY', '5h42cgakvq6ovrsm7rd6dtm2rj')
+zap = ZAPv2(apikey=api_key, proxies=proxies)
+target_url = 'http://pd-net-prod-main-1518350640.us-east-1.elb.amazonaws.com/'
+scan_id = zap.spider.scan(target_url)
 
-try:
-    # Dummy request to the proxy server to check connectivity
-    response = requests.get('http://pd-net-prod-main-1518350640.us-east-1.elb.amazonaws.com/', proxies=proxies)
-    print(f"Proxy connection successful. Response: {response.text}")
-    api_key = os.getenv('ZAP_API_KEY', '5h42cgakvq6ovrsm7rd6dtm2rj')
-    zap = ZAPv2(apikey=api_key, proxies=proxies)
-    target_url = 'http://pd-net-prod-main-1518350640.us-east-1.elb.amazonaws.com/'
-    scan_id = zap.spider.scan(target_url)
+print(f'Spidering target {target_url}...')
+while int(zap.spider.status(scan_id)) < 100:
+    print(f'Spider progress %: {zap.spider.status(scan_id)}')
+    time.sleep(1)
 
-    print(f'Spidering target {target_url}...')
-    while int(zap.spider.status(scan_id)) < 100:
-        print(f'Spider progress %: {zap.spider.status(scan_id)}')
-        time.sleep(1)
-
-    print('Spider has completed!')
-    print('\n'.join(map(str, zap.spider.results(scan_id))))
-
-except Exception as e:
-    print(f"Failed to connect to the proxy. Error: {e}")
-
-# Rest of your script...
+print('Spider has completed!')
+print('\n'.join(map(str, zap.spider.results(scan_id))))
