c1074b77fb8aec42fa6f43381bb5275176ab92d0
manojnaik12 <141448013+manojnaik12@users.noreply.github.com>
2024-01-15 00:02:42 +0530

    Create zap_script.py

---
+++ zap_script.py
@@ -0,0 +1,23 @@
+# zap_script.py
+import os
+from zapv2 import ZAPv2
+
+# ZAP API URL
+zap_api_url = os.getenv('ZAP_API_URL', 'http://localhost:8081/')  # Update with your ZAP API URL
+
+# ZAP API Key
+api_key = os.getenv('ZAP_API_KEY', '5h42cgakvq6ovrsm7rd6dtm2rj')  # Update with your ZAP API key
+
+# Initialize ZAP
+zap = ZAPv2(apikey=api_key, baseurl=zap_api_url)
+
+# Your ZAP-related actions here
+# For example, you can run a spider scan or an active scan
+
+# Example: Spider the target URL
+target_url = 'http://pd-net-prod-main-1518350640.us-east-1.elb.amazonaws.com/'
+scan_id = zap.spider.scan(target_url)
+
+while int(zap.spider.status(scan_id)) < 100:
+    print(f'Spider progress %: {zap.spider.status(scan_id)}')
+    time.sleep(1)
