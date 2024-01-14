edbcbb4086eab33ce24dc691395afb047aeb19e3
manojnaik12 <141448013+manojnaik12@users.noreply.github.com>
2024-01-15 00:17:49 +0530

    Update zap_script.py

--- zap_script.py
+++ zap_script.py
@@ -12,6 +12,19 @@ try:
     # Dummy request to the proxy server to check connectivity
     response = requests.get('http://pd-net-prod-main-1518350640.us-east-1.elb.amazonaws.com/', proxies=proxies)
     print(f"Proxy connection successful. Response: {response.text}")
+    api_key = os.getenv('ZAP_API_KEY', '5h42cgakvq6ovrsm7rd6dtm2rj')
+    zap = ZAPv2(apikey=api_key, proxies=proxies)
+    target_url = 'http://pd-net-prod-main-1518350640.us-east-1.elb.amazonaws.com/'
+    scan_id = zap.spider.scan(target_url)
+
+    print(f'Spidering target {target_url}...')
+    while int(zap.spider.status(scan_id)) < 100:
+        print(f'Spider progress %: {zap.spider.status(scan_id)}')
+        time.sleep(1)
+
+    print('Spider has completed!')
+    print('\n'.join(map(str, zap.spider.results(scan_id))))
+
 except Exception as e:
     print(f"Failed to connect to the proxy. Error: {e}")
 
